fe783772fac1b21b8487dbb3721239a93992c7d5
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2025-09-16 13:14:51 +0530

    added delete content route

--- backend/src/routes/contentRoutes.ts
+++ backend/src/routes/contentRoutes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { createContentSchema, RequestBodyContent } from "../zod-validation/contentSchema";
 import ContentModel from "../models/contentModel";
 import authentication from "../middleware/userAuthentication";
+import mongoose from "mongoose";
 
 const router = Router();
 
@@ -36,7 +37,7 @@ router.post("/content", authentication, async(req: Request<{},{},RequestBodyCont
 router.get("/content", authentication, async(req,res)=>{
      try{
           const allContent = await ContentModel.find({userId : req.user_info?.user_id}).populate({path : "userId", select: "firstName"});
-          
+
           if(allContent.length !==0){
                 return res.send({
                      msg : "user contents found successfully",
@@ -51,15 +52,65 @@ router.get("/content", authentication, async(req,res)=>{
                 })
           }
      }catch(err){
-          if(err instanceof Error){
                return res.status(500).send({
                     msg : "failed to find the contents",
                     success : false,
-                    detailError : err.message
+                    detailError : err instanceof Error ? err.message : err
                })
           }
-     }
-})
+});
+
+
+router.delete("/content/:contentId", authentication, async (req: Request<{ contentId: string }>, res: Response) => {
+
+    const { contentId } = req.params;
+
+    if (!contentId) {
+      return res.status(400).json({
+        msg: "Invalid DELETE request",
+        success: false,
+        detailError: "Request param `contentId` is missing",
+      });
+    }
+
+    if (!mongoose.isValidObjectId(contentId)) {
+      return res.status(400).json({
+        msg: "Invalid contentId",
+        success: false,
+        detailError: "Provided contentId is not a valid ObjectId",
+      });
+    }
+
+    try {
+      const deletedContent = await ContentModel.findOneAndDelete({_id : contentId});
+
+      if (!deletedContent) {
+        return res.status(404).json({
+          msg: "Content not found",
+          success: false,
+          detailError: "No content with the provided contentId",
+        });
+      }
+
+      return res.json({
+        msg: "Content deleted successfully",
+        success: true,
+        content: deletedContent,
+      });
+
+    } 
+    catch (err) {
+      return res.status(500).json({
+        msg: "Failed to delete content",
+        success: false,
+        detailError: err instanceof Error ? err.message : err,
+      });
+    }
+});
+
+
+
+
 
 
 export default router;
